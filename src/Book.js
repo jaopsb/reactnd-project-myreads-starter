ebb6744abd227c32c97d5f6d8dd33b1110c6a6c3
jaopsb <jpsbdbz@gmail.com>
2018-12-12 17:42:37 -0200

    adjusting SearchPage

--- src/Book.js
+++ src/Book.js
@@ -1,27 +1,36 @@
 import React from 'react';
 //import PropTypes from 'prop-types';
 
-const Book = ({ id, title, author, bookURL, status, onChange }) => (
-  <li>
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${bookURL})` }}></div>
-        <div className="book-shelf-changer">
-          <select value={status} onChange={(e) => {
-            onChange(e, id);
-          }
-          }>
-            <option value="move" disabled>Move to...</option>
-            <option value="CURRENT">Currently Reading</option>
-            <option value="WANT">Want to Read</option>
-            <option value="READ">Read</option>
-          </select>
+const Book = (props) => {
+  const { id, title, author, imageLinks, status, onChange } = props;
+  let thumbnail = " ";
+
+  if (imageLinks) {
+    thumbnail = imageLinks.thumbnail;
+  }
+
+  return (
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: thumbnail !== " " ? `url(${thumbnail})` : "none" }}></div>
+          <div className="book-shelf-changer">
+            <select value={status} onChange={(e) => {
+              onChange(e, id);
+            }
+            }>
+              <option value="move" disabled>Move to...</option>
+              <option value="CURRENT">Currently Reading</option>
+              <option value="WANT">Want to Read</option>
+              <option value="READ">Read</option>
+            </select>
+          </div>
         </div>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">{author}</div>
       </div>
-      <div className="book-title">{title}</div>
-      <div className="book-authors">{author}</div>
-    </div>
-  </li >
-);
+    </li >
+  );
+}
 
 export default Book;
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI'
+import BookShelf from './BookShelf';
 
 class SearchPage extends React.Component {
+  state = {
+    books: []
+  }
+  onChange = (e) => {
+    let str = e.target.value;
+
+    if (str !== "") {
+      BooksAPI.search(str)
+        .then(books => {
+
+          if (!books.error) {
+            this.setState({ books })
+          } else {
+            this.setState({ books: [] });
+          }
+        })
+        .catch(error => console.log(error));
+    } else {
+      this.setState({ books: [] });
+    }
+  }
   render() {
     return (
       <div className="search-books">
@@ -16,12 +39,18 @@ class SearchPage extends React.Component {
             However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
             you don't find a specific author or title. Every search is limited by search terms.
           */}
-            <input type="text" placeholder="Search by title or author" />
+            <input type="text" onChange={this.onChange} placeholder="Search by title or author" />
 
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid"></ol>
+          {this.state.books &&
+            <BookShelf
+              onChangeStatus={(e, id) => console.log("status changed")}
+              title={""}
+              books={this.state.books}
+            />}
         </div>
       </div>
     );
